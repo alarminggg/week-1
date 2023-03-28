5c40b718edd272de160e64fd2c75fabd8e33a3f7
alarminggg <124152367+alarminggg@users.noreply.github.com>
2023-03-28 14:49:59 +0100

    patrolling

---
+++ Assets/Patrolling/Scripts/DroneMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DroneMove : MonoBehaviour
+{
+    [SerializeField]
+    Transform destination;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        if(navMeshAgent == null)
+        {
+            Debug.LogError("The NavMeshAgent component is not attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+
+    void SetDestination()
+    {
+        if(destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+    
+    
+}
---
+++ Assets/Patrolling/Scripts/DronePatrol.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DronePatrol : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    GameObject currentWaypoint;
+    GameObject previousWaypoint;
+    GameObject[] allWaypoints;
+    bool travelling;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        currentWaypoint = GetRandomWaypoint();
+        SetDestination();
+    }
+
+    
+    void Update()
+    {
+        if(travelling && navMeshAgent.remainingDistance <= 1f)
+        {
+            travelling = false;
+            SetDestination();
+        }
+    }
+
+    private void SetDestination()
+    {
+        previousWaypoint = currentWaypoint;
+        currentWaypoint = GetRandomWaypoint();
+
+        Vector3 targetVector = currentWaypoint.transform.position;
+        navMeshAgent.SetDestination(targetVector);
+        travelling = true;
+
+    }
+
+    public GameObject GetRandomWaypoint()
+    {
+        if(allWaypoints.Length == 0)
+        {
+            return null;
+        }
+        else
+        {
+            int index = Random.Range(0, allWaypoints.Length);
+            return allWaypoints[index];
+        }
+    }
+}
