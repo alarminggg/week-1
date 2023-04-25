51d324cea46514030648b07f0f3382ce30198ebb
alarminggg <124152367+alarminggg@users.noreply.github.com>
2023-04-25 16:16:17 +0100

    25/4/23

--- Assets/Enemy/Scripts/EnemyAttack.cs
+++ Assets/Enemy/Scripts/EnemyAttack.cs
@@ -7,9 +7,7 @@ public class EnemyAttack : MonoBehaviour
     [SerializeField]
     Transform playerTransform;
     Transform gunTransform;
-    [SerializeField]
     float maxDistanceToTarget = 6f;
-
     float distanceToTarget;
 
     [SerializeField]
@@ -20,66 +18,65 @@ public class EnemyAttack : MonoBehaviour
     float tick;
     bool attackReady = true;
 
-
-    private void Start()
+    void Start()
     {
         tick = delayTimer;
         gunTransform = gameObject.transform.Find("Gun");
     }
 
-    private void Update()
+    void Update()
     {
         Attack();
     }
 
-    void Attack()
+    bool IsReadyToAttack()
     {
-        distanceToTarget = Vector3.Distance(playerTransform.position, transform.position);
-        attackReady = IsAttackReady();
-
-        if(distanceToTarget <= maxDistanceToTarget)
-        {
-            LookAtTarget();
-            if(attackReady)
-            {
-                tick = 0f;
-                Ray ray = new Ray(gunTransform.transform.position, gunTransform.transform.forward);
-                Debug.DrawRay(gunTransform.transform.position, gunTransform.transform.forward * 3, Color.magenta, 0.2f);
-
-                if (Physics.Raycast(ray, out RaycastHit raycastHit, maxDistanceToTarget))
-                {
-                    Debug.Log("Enemy Shoots");
-                    if(raycastHit.transform != null)
-                    {
-                        raycastHit.collider.SendMessageUpwards("Hit", rawDamage, SendMessageOptions.DontRequireReceiver);
-                    }
-                    
-                }
-                else
-                {
-                    Debug.Log("Enemy: Failed to return value on raycast");
-                }
-            }
-        }
-    }
-    
-    void LookAtTarget()
-    {
-        //ransform.LookAt(playerTransform.position);
-        Vector3 lookVector = playerTransform.position - transform.position;
-        lookVector.y = transform.position.y;
-        Quaternion rotation = Quaternion.LookRotation(lookVector);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.1f);
-    }
-
-    bool IsAttackReady()
-    {
-        if(tick < delayTimer)
+        if (tick < delayTimer)
         {
             tick += Time.deltaTime;
             return false;
         }
 
         return true;
+    }
+
+    void LookAtTarget()
+    {
+        //this.transform.LookAt(playerTransform.position);
+        Vector3 lookVector = playerTransform.position - transform.position;
+        lookVector.y = transform.position.y;
+        Quaternion rotation = Quaternion.LookRotation(lookVector);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.01f);
+    }
+
+    void Attack()
+    {
+        distanceToTarget = Vector3.Distance(playerTransform.position, gunTransform.position);
+        attackReady = IsReadyToAttack();
+
+        if (distanceToTarget <= maxDistanceToTarget)
+        {
+            LookAtTarget();
+
+            if (attackReady)
+            {
+                tick = 0f;
+                Ray ray = new Ray(gunTransform.position, gunTransform.forward);
+                RaycastHit raycastHit;
+
+                if (Physics.Raycast(ray, out raycastHit, maxDistanceToTarget))
+                {
+                    Debug.Log("Enemy Shoots");
+                    if (raycastHit.transform != null)
+                    {
+                        raycastHit.collider.SendMessageUpwards("Hit", rawDamage, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+                else
+                {
+                    Debug.Log("ENEMY: FAILED RAYCAST");
+                }
+            }
+        }
     }
 }
