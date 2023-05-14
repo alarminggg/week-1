225f28939ee0af3c909c4a4a154e898eb4482121
alarminggg <124152367+alarminggg@users.noreply.github.com>
2023-05-14 20:15:56 +0100

    level finished

--- Assets/Environment/Script/MovingPlatform.cs
+++ Assets/Environment/Script/MovingPlatform.cs
@@ -22,17 +22,35 @@ public class MovingPlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        TargetNextWaypoint();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        elapsedTime += Time.deltaTime;
+
+        float elapsedPercentage = elapsedTime / timeToWaypoint;
+        elapsedPercentage = Mathf.SmoothStep(0, 1, elapsedPercentage);
+        transform.position = Vector3.Lerp(previousWaypoint.position, targetWaypoint.position, elapsedPercentage);
+
+        if (elapsedPercentage >= 1)
+        {
+            TargetNextWaypoint();
+        }
     }
 
     private void TargetNextWaypoint()
     {
-        previousWaypoint = waypointPath
+        previousWaypoint = waypointPath.GetWaypoint(targetWaypointIndex);
+        targetWaypointIndex = waypointPath.GetNextWaypointIndex(targetWaypointIndex);
+        targetWaypoint = waypointPath.GetWaypoint(targetWaypointIndex);
+
+        elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(previousWaypoint.position, targetWaypoint.position);
+        timeToWaypoint = distanceToWaypoint / speed;
     }
+
+    
 }
